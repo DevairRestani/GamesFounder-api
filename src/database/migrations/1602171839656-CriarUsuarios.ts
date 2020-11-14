9775bff182d93d4d4bd62f762d6d83db6fef4c8e
Devair Restani <devair.juca2013@gmail.com>
2020-11-13 21:56:19 -0300

    adicionar extenção uuid ao banc

--- src/database/migrations/1602171839656-CriarUsuarios.ts
+++ src/database/migrations/1602171839656-CriarUsuarios.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CriarUsuarios1602171839656 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
         await queryRunner.createTable(
             new Table({
                 name: "usuarios",
